13e932b69a043b8ffed35b79ba342d603688187d
antolabarca <anto.labarca.s@gmail.com>
2020-09-22 19:57:37 -0300

    comparacion de gamma con factorial

---
+++ gamma.py
@@ -0,0 +1,62 @@
+from funciones_gamma import g
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+import math
+
+eps = sys.float_info.epsilon #el numero mas pequeno que es mayor a 0, para usarlo en vez de 0 al dividir
+
+def gamma_n(z,n):
+    """
+    Calcula la funcion Gamma(z) para n intervalos, usando la regla de Simpson
+    Se integra considerando el cambio de variable u = 1/(1+x) para integrar entre 0 y 1
+
+    Params:
+        z: el valor para el que se quiere calcular Gamma
+        n: la cantidad de intervalos
+    """
+    delta_x = 1.0/n
+    s = 0
+    s += g(eps,z) #se hace desde eps en vez de 0 directamente para evitar error de divide by zero
+    #De todas formas la funcion era bastante plana en esa parte, por lo que la diferencia es pequena
+    for i in range(1, n-1):
+        g2i = g(2*i*delta_x, z)
+        s +=2*g2i
+    for i in range(1, n-1):
+        g2i1 = g((2*i + 1)*delta_x, z)
+        s +=4*g2i1
+    s +=g(1, z)
+    return s*(delta_x/3)
+
+def gamma(z, tol):
+    """
+    Calcula la funcion Gamma(z) para una cierta tolerancia, usando la regla de Simpson y la funcion gamma_n para cada iteracion
+    Se integra considerando el cambio de variable u = 1/(1+x) para no tener integrales infinitas
+
+    Params:
+        z: el valor para el que se quiere calcular Gamma
+        eps: la precision relativa deseada
+    """
+    n=8
+    In = gamma_n(z,n)
+    I2n = gamma_n(z, 2*n)
+
+    while abs((I2n - In)/In) >= tol:
+        n *=2
+        In = I2n
+        I2n = gamma_n(z, 2*n)
+    
+    return I2n
+
+z = np.linspace(0, 12, num=100)
+z_int = np.arange(13)
+
+for i in range(1,12):
+    gam=gamma(i, 0.0001)
+    fact=math.factorial(i-1)
+    print("gamma("+str(i)+"): ")
+    print(gam)
+    print(str(i)+"-1! :")
+    print(fact)
+    print("Diferencia:" + str(abs(gam-fact)))
+    print(" ")
