a791e7d8b26550d8791e47dfee2aedd0aaee710c
antolabarca <anto.labarca.s@gmail.com>
2020-09-23 23:55:09 -0300

    agregar funcion que calcula f(a) iterando sobre la canidad de intervalos hasta lograr precision

--- f.py
+++ f.py
@@ -23,3 +23,24 @@ def f_n(a,n):
         s +=4*chi2i1
     s +=chi2(a)
     return s*(delta_x/3)
+
+
+def f(a, tol):
+    """
+    Calcula la integral de chi^2 desde 0 hasta a para una cierta tolerancia, usando la regla de Simpson
+    y la funcion f_n para cada iteracion
+
+    Params:
+        :param a: el valor para el que se quiere calcular f(a), es decir, el lim superior de la integral
+        :param tol: la precision relativa deseada
+    """
+    n=8
+    fn = f_n(z,n)
+    f2n = f_n(z, 2*n)
+
+    while abs((f2n - fn)/fn) >= tol:
+        n *=2
+        fn = f2n
+        f2n = f_n(z, 2*n)
+    
+    return f2n
